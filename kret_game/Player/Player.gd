23c969c9ff60a6ccd6374c3679e9a37ab7658073
adamnobody <uliya.ru3456@gmail.com>
2022-10-11 10:16:11 +0300

    Created anims

--- kret_game/Player/Player.gd
+++ kret_game/Player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 
 export (int) var speed
+onready var animatedSprite = $AnimatedSprite
 var velocity = Vector2()
 
 
@@ -10,16 +11,23 @@ func _ready():
 
 
 func getInput():
+	animatedSprite.flip_h = false
 	if Input.is_action_pressed("playerMovementUp"):
 		velocity = Vector2.UP
+		animatedSprite.play("run")
 	elif Input.is_action_pressed("playerMovementDown"):
 		velocity = Vector2.DOWN
+		animatedSprite.play("run")
 	elif Input.is_action_pressed("playerMovementLeft"):
 		velocity = Vector2.LEFT
+		animatedSprite.play("run")
+		animatedSprite.flip_h = true
 	elif Input.is_action_pressed("playerMovementRight"):
 		velocity = Vector2.RIGHT
+		animatedSprite.play("run")
 	else:
 		velocity = Vector2.ZERO
+		animatedSprite.play("idle")
 		
 	velocity = velocity.normalized() * speed
 
