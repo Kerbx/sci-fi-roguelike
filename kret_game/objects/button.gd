d824772ab2c1f92fdd6053ad3f9a6bf393d69be1
Kerbx <r3ostat@ya.ru>
2022-10-14 20:19:50 +0200

    Created simple functional for newgame button.

---
+++ kret_game/objects/button.gd
@@ -0,0 +1,23 @@
+extends Button
+
+export var referencePath = ""
+export(bool) var startFocused = false
+
+
+func _ready():
+	if(startFocused):
+		grab_focus()
+	
+	connect("mouse_entered", self, "_onButtonMouseEntered")
+	connect("pressed", self, "_onButtonPressed")
+	
+	
+func _onButtonMouseEntered():
+	grab_focus()
+
+
+func _onButtonPressed():
+	if(referencePath != ""):
+		get_tree().change_scene(referencePath)
+	else:
+		get_tree().quit()
